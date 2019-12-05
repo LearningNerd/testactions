772ed8ed482eb298156a84fa1ca5832928fef516
Liz Krane <learningnerd@gmail.com>
2019-12-05 15:52:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log("/ endpoint hit");
-  res.send('Hello World!');
+  res.send('Hi this is new');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
