f4746300e9b0d14a384b2a02659fc1da3f341b8c
Liz Krane <learningnerd@gmail.com>
2019-12-05 10:24:16 -0800

    express

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("Hey!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log("/ endpoint hit");
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
